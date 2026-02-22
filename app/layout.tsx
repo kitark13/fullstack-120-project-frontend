55d8bfc4f23936c77129d2dc88898b473510ccd0
kitark13 <kratykd@icloud.com>
2026-02-22 22:14:31 +0100

    unbounded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from "next";
-import { Nunito_Sans, Sora } from "next/font/google";
+import { Unbounded, Nunito_Sans, Sora } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
 
-const nunitoSans = Nunito_Sans({
-  subsets: ["cyrillic"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-nunito-sans",
+import {} from "next/font/google";
+
+const unbounded = Unbounded({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "600", "800"],
+  variable: "--font-heading",
   display: "swap",
 });
 
+// const nunitoSans = Nunito_Sans({
+//   subsets: ["cyrillic"],
+//   weight: ["400", "500", "600", "700", "800", "900"],
+//   variable: "--font-nunito-sans",
+//   display: "swap",
+// });
+// ${nunitoSans.variable}
 const sora = Sora({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
@@ -30,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body className={`${nunitoSans.variable} ${sora.variable}`}>
+      <body className={`${unbounded.variable} ${sora.variable}`}>
         <TanStackProvider>{children}</TanStackProvider>
       </body>
     </html>

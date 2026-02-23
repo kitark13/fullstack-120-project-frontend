32252a835a1f588f03fbaff08d0896229bd60b6b
kitark13 <kratykd@icloud.com>
2026-02-24 00:08:39 +0100

    connect backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // import css from "./page.module.css";
 import Join from "@/components/home/Join/Join";
+import Istorii from "@/components/Istorii/Istorii";
 
 export default function Home() {
   return (
     <main>
       <Join />
+      <Istorii />
     </main>
   );
 }

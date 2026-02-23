2697874838b6720a5ea049f8f6578736c7d8e526
kitark13 <kratykd@icloud.com>
2026-02-23 17:09:50 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import type { Metadata } from "next";
-import { Unbounded, Nunito_Sans, Sora } from "next/font/google";
+import "modern-normalize/modern-normalize.css";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
 import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
 import Header from "../components/layout/Header/Header";
 import { Footer } from "../components/layout/Footer/Footer";
 
+// Unbounded
 // const unbounded = Unbounded({
 //   subsets: ["latin", "cyrillic"],
 //   weight: ["400", "600", "800"],
@@ -12,24 +14,28 @@ import { Footer } from "../components/layout/Footer/Footer";
 //   display: "swap",
 // });
 // ${unbounded.variable}
+
 const nunitoSans = Nunito_Sans({
-  subsets: ["cyrillic"],
+  subsets: ["latin", "cyrillic"],
   weight: ["400", "500", "600", "700", "800", "900"],
   variable: "--font-nunito-sans",
   display: "swap",
 });
 
-const sora = Sora({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800"],
-  variable: "--font-sora",
-  display: "swap",
-});
+// Sora
+// const sora = Sora({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700", "800"],
+//   variable: "--font-sora",
+//   display: "swap",
+// });
+// ${sora.variable}
 
 export const metadata: Metadata = {
   title: "Подорожники",
   description:
     "Подорожники — додаток для збереження та перегляду ваших подорожей",
+  icons: { icon: "/favicon-opt.svg" },
 };
 
 export default function RootLayout({
@@ -39,11 +45,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body className={`${nunitoSans.variable} ${sora.variable}`}>
+      <body className={`${nunitoSans.variable}`}>
         <TanStackProvider>
-          <Header />
-          {children}
-          <Footer />
+          <div className="page">
+            <Header />
+            {children}
+            <Footer />
+          </div>
         </TanStackProvider>
       </body>
     </html>

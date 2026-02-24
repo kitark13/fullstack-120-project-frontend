bee5cf5105597ecdeb55b4dd85a634d8f6f99aa1
Dari <dariatsevashova@gmail.com>
2026-02-24 09:12:35 +0100

    feat: add login-form with auth provider

--- components/forms/LoginForm/LoginForm.tsx
+++ components/forms/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useRouter } from "next/navigation";
 import Link from "next/link";

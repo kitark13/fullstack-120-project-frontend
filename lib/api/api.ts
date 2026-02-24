ecebae62be1207441b5d0d0e6577bb4f63d77a87
kitark13 <kratykd@icloud.com>
2026-02-25 00:23:26 +0100

    fix bugs cookies v2

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   //   baseURL: "http://localhost:3000",
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   withCredentials: true,
 });
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://fullstack-120-project-group-1-backend.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

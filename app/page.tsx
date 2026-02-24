8a01d1c9b4a203b1dd29e9d751703f5c0307c250
Eternal <gracious0603@gmail.com>
2026-02-24 20:29:41 +0100

    SectionAbout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // import css from "./page.module.css";
-import Hero from "@/components/home/Hero/Hero";
-import Join from "@/components/home/Join/Join";
-import Istorii from "@/components/Istorii/Istorii";
+import About from '@/components/home/About/About';
+import Hero from '@/components/home/Hero/Hero';
+import Join from '@/components/home/Join/Join';
+import Istorii from '@/components/Istorii/Istorii';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
       <Join />
       <Istorii />
     </main>

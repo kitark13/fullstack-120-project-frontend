7d2a867a24e7865a1649568f5a6006ff0299c668
kitark13 <kratykd@icloud.com>
2026-02-25 14:15:28 +0100

    fix api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
   //   baseURL: "http://localhost:3000",
-  // baseURL: "/api",
-  // withCredentials: true,
-  baseURL: 'https://fullstack-120-project-group-1-backend.onrender.com/', //process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
+  //process.env.NEXT_PUBLIC_API_URL
+  // baseURL: "https://fullstack-120-project-group-1-backend.onrender.com/",
   withCredentials: true,
 });

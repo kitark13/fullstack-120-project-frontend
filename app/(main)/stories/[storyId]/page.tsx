48df9177ac8f186d92985593484fa203a4e9e703
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-27 22:28:40 +0200

    implement story page with details and popular stories

---
+++ app/(main)/stories/[storyId]/page.tsx
@@ -0,0 +1,31 @@
+import { PopularStories } from "@/components/stories/PopularStories/PopularStories";
+import StoryDetails from "@/components/stories/StoryDetails/StoryDetails";
+import { getStoryByIdServer } from "@/lib/api/serverApi";
+import { notFound } from "next/navigation";
+import styles from "./StoryPage.module.css";
+
+interface Props {
+  params: Promise<{ storyId: string }>;
+}
+
+export default async function StoryPage({ params }: Props) {
+  const { storyId } = await params;
+
+  const storyRes = await getStoryByIdServer(storyId);
+
+  if (!storyRes || !storyRes.data) {
+    notFound();
+  }
+
+  return (
+    <>
+      <StoryDetails story={storyRes.data} />
+      {/* <StoryDetails story={storyRes.data} isSaved={storyRes.isSaved} /> */}
+      <section className={styles.popularStories}>
+        <div className="container">
+          <PopularStories />
+        </div>
+      </section>
+    </>
+  );
+}

9a482c90679d6a29d4f2fd2f5f00f8f86536cb49
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-03-03 07:28:03 +0200

    add AuthNavModal

--- components/stories/StoryDetails/SaveButton.client.tsx
+++ components/stories/StoryDetails/SaveButton.client.tsx
@@ -5,6 +5,8 @@ import { addToFavorite, removeFromFavorite } from "@/lib/api/clientApi";
 import useAuthStore from "@/lib/store/authStore";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import AuthNavModal from "@/components/modals/AuthNavModal/AuthNavModal";
 
 interface SaveButtonProps {
   storyId: string;
@@ -19,28 +21,29 @@ export default function SaveButton({
 }: SaveButtonProps) {
   const router = useRouter();
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const mutation = useMutation({
     mutationFn: (shouldSave: boolean) =>
       shouldSave ? addToFavorite(storyId) : removeFromFavorite(storyId),
 
     onMutate: (shouldSave) => {
-      setIsSaved(shouldSave); //  миттєве оновлення UI
+      setIsSaved(shouldSave);
     },
 
     onError: (_, shouldSave) => {
-      setIsSaved(!shouldSave); //  rollback
+      setIsSaved(!shouldSave);
       alert("Помилка. Спробуйте ще раз.");
     },
 
     onSuccess: () => {
-      router.refresh(); // щоб серверні дані підтягнулись
+      router.refresh();
     },
   });
 
   const handleClick = () => {
     if (!isAuthenticated) {
-      alert("Щоб зберегти статтю, потрібно увійти.");
+      setIsModalOpen(true);
       return;
     }
 
@@ -48,14 +51,34 @@ export default function SaveButton({
     mutation.mutate(shouldSave);
   };
 
+  const handleLogIn = () => {
+    setIsModalOpen(false);
+    router.push("/auth/login");
+  };
+
+  const handleRegister = () => {
+    setIsModalOpen(false);
+    router.push("/auth/register");
+  };
+
   return (
-    <Button
-      type="button"
-      variant={isSaved ? "saved" : "primary"}
-      onClick={handleClick}
-      disabled={mutation.isPending}
-    >
-      {mutation.isPending ? "..." : isSaved ? "Збережено" : "Зберегти"}
-    </Button>
+    <>
+      <Button
+        type="button"
+        variant={isSaved ? "saved" : "primary"}
+        onClick={handleClick}
+        disabled={mutation.isPending}
+      >
+        {mutation.isPending ? "..." : isSaved ? "Збережено" : "Зберегти"}
+      </Button>
+      <AuthNavModal
+        isOpen={isModalOpen}
+        onLogIn={handleLogIn}
+        onRegister={handleRegister}
+        onClose={() => setIsModalOpen(false)}
+        title="Авторизація"
+        message="Щоб зберегти статтю у вибране, вам необхідно увійти в систему або зареєструватися."
+      />
+    </>
   );
 }

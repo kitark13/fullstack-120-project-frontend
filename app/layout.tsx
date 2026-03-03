fb1aaddbea268616ae6c2679183d2586f435557e
kitark13 <kratykd@icloud.com>
2026-03-03 18:52:19 +0100

    update opengraph img

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
     url: "https://fullstack-120-project-frontend.vercel.app",
     images: [
       {
-        url: "/images/hero/hero-desktop-1x.jpg",
+        url: "/opengraph-img.jpg",
         width: 1200,
         height: 630,
         alt: "Подорожники — додаток для мандрівників",
